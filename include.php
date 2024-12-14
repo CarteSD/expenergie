a8119120a049e70a61f2db85ad5766e043032bd0
Estéban DESESSARD <edesessard@iutbayonne.univ-pau.fr>
2024-12-14 17:56:27 +0100

    :art: Lancement du projet

---
+++ include.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * @file     include.php
+ * @author   Estéban DESESSARD
+ * @brief
+ * @details
+ * @date     14/12/2024
+ * @version  0.0
+ */
---
+++ public/assets/index.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * @file     index.php
+ * @author   Estéban DESESSARD
+ * @brief
+ * @details
+ * @date     14/12/2024
+ * @version  0.0
+ */

4d866bcd9bc188662fcf380a271863a5bf1e960c
Estéban DESESSARD <edesessard@iutbayonne.univ-pau.fr>
2024-12-15 10:58:53 +0100

    :sparkles: Ajout des polices d'écritures

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["./src/**/*.{js,html,php,twig}"],
+    theme: {
+        fontFamily: {
+            'serif': ["'Urbanist'", "ui-serif", "Georgia", "Cambria", "Times New Roman", "Times", "serif"],
+            'sans': ["'Barlow Condensed'", "ui-sans-serif", "system-ui", "sans-serif", "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji"]
+        },
+    },
+}
+

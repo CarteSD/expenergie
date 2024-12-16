d2ca892cd6c1b15962d2488a80bc3b9a8a742f67
Estéban DESESSARD <edesessard@iutbayonne.univ-pau.fr>
2024-12-16 17:52:05 +0100

    :sparkles: Ajout des fenêtres modales de précisions

---
+++ public/assets/js/offres.js
@@ -0,0 +1,18 @@
+function closeModal() {
+    let background = document.getElementById('modal-background');
+    let modals = document.querySelectorAll('.modal');
+    modals.forEach(modal => {
+        if (!modal.classList.contains('hidden')) {
+            modal.classList.add("hidden");
+        }
+    })
+    background.classList.add("hidden");
+
+}
+
+function showModal(e) {
+    let background = document.getElementById('modal-background');
+    let modal = document.getElementById(`modal_${e.id}`);
+    modal.classList.remove("hidden");
+    background.classList.remove("hidden");
+}

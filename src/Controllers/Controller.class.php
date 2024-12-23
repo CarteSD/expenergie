ecb2d242651fd9819323bba85b0de98903043cd4
Estéban DESESSARD <edesessard@iutbayonne.univ-pau.fr>
2024-12-23 13:36:41 +0100

    :pencil2: Ajout des méthodes d'encapsulation

--- src/Controllers/Controller.class.php
+++ src/Controllers/Controller.class.php
@@ -11,6 +11,7 @@
 namespace Controllers;
 
 use Db;
+use Exception;
 use PDO;
 use Twig\Environment;
 use Twig\Loader\FilesystemLoader;
@@ -40,5 +41,55 @@ class Controller
         if (!empty($_POST)) {
             $this->post = $_POST;
         }
+    }
+
+    public function getPdo(): PDO
+    {
+        return $this->pdo;
+    }
+
+    public function setPdo(PDO $pdo): void
+    {
+        $this->pdo = $pdo;
+    }
+
+    public function getLoader(): FilesystemLoader
+    {
+        return $this->loader;
+    }
+
+    public function setLoader(FilesystemLoader $loader): void
+    {
+        $this->loader = $loader;
+    }
+
+    public function getTwig(): Environment
+    {
+        return $this->twig;
+    }
+
+    public function setTwig(Environment $twig): void
+    {
+        $this->twig = $twig;
+    }
+
+    public function getGet(): ?array
+    {
+        return $this->get;
+    }
+
+    public function setGet(?array $get): void
+    {
+        $this->get = $get;
+    }
+
+    public function getPost(): ?array
+    {
+        return $this->post;
+    }
+
+    public function setPost(?array $post): void
+    {
+        $this->post = $post;
     }
 }

a810089ffd68c0a057790be2acb43b70f84a3d95
Estéban DESESSARD <edesessard@iutbayonne.univ-pau.fr>
2024-12-23 13:31:04 +0100

    :sparkles: Ajout des constantes relatives à la base de données

---
+++ config/db.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * @file     db.php
+ * @author   Estéban DESESSARD
+ * @brief
+ * @details
+ * @date     23/12/2024
+ * @version  1.0
+ */
+
+define('DB_TYPE', $_ENV['DB_TYPE']);
+define('DB_HOST', $_ENV['DB_HOST']);
+define('DB_PORT', $_ENV['DB_PORT']);
+define('DB_NAME', $_ENV['DB_NAME']);
+define('DB_USER', $_ENV['DB_USER']);
+define('DB_PASS', $_ENV['DB_PASS']);
+define('DB_PREFIX', $_ENV['DB_PREFIX']);

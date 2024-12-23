0b6189903bb51c74b0963d16dacb15e83046d446
Estéban DESESSARD <edesessard@iutbayonne.univ-pau.fr>
2024-12-23 14:29:23 +0100

    :sparkles: Gestion de l'envoi du formulaire de contact avec le  `ControllerContact`

--- src/Controllers/ControllerContact.class.php
+++ src/Controllers/ControllerContact.class.php
@@ -10,6 +10,7 @@
 
 namespace Controllers;
 
+use PHPMailer\PHPMailer\PHPMailer;
 use QuestionDAO;
 use Twig\Environment;
 use Twig\Loader\FilesystemLoader;
@@ -23,5 +24,64 @@ class ControllerContact extends Controller
 
     public function showContactPage() {
         echo $this->getTwig()->render('contact.twig');
+    }
+
+    public function sendContactForm() {
+        if (empty($_POST['nom']) || empty($_POST['prenom']) || empty($_POST['zipcode']) || empty($_POST['ville']) || empty($_POST['email']) || empty($_POST['message'] || !isset($_POST['accept']))) {
+            echo $this->getTwig()->render('contact.twig', ['error' => 'Tous les champs sont obligatoires.']);
+            exit;
+        }
+        if (!filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
+            echo $this->getTwig()->render('contact.twig', ['error' => 'L\'adresse email n\'est pas valide.']);
+            exit;
+        }
+        if (strlen($_POST['message']) < 32) {
+            echo $this->getTwig()->render('contact.twig', ['error' => 'Le message doit contenir au moins 32 caractères.']);
+            exit;
+        }
+        $mail = new PHPMailer();
+        $mail->isSMTP();
+        $mail->SMTPDebug = 0;
+        $mail->Host = 'partage.univ-pau.fr';
+        $mail->SMTPAuth = true;
+        $mail->Username = $_ENV['MAIL_USER'];
+        $mail->Password = $_ENV['MAIL_PASS'];
+        $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;
+        $mail->Port = 465;
+        $mail->CharSet = 'UTF-8';
+        $mail->Encoding = 'base64';
+
+        $mail->setFrom('edesessard@univ-pau.fr', $_POST['nom'] . ' ' . $_POST['prenom']);
+        $mail->addAddress('edesessard@iutbayonne.univ-pau.fr', 'Estéban DESESSARD');
+        $mail->isHTML(true);
+        $mail->Subject = "Nouveau message depuis le formulaire de contact";
+        $mail->Body = "Message reçu de : " . $_POST['nom'] . " " . $_POST['prenom'] . " (" . $_POST['email'] . ")<br>Code postal : " . $_POST['zipcode'] . ", Ville : " . $_POST['ville'] . "<br><br>" . nl2br(htmlspecialchars($_POST['message'] ));
+        $mail->setLanguage('fr', './vendor/phpmailer/phpmailer/language/phpmailer.lang-fr.php');
+
+        $mailConfirm = new PHPMailer();
+        $mailConfirm->isSMTP();
+        $mailConfirm->SMTPDebug = 0;
+        $mailConfirm->Host = 'partage.univ-pau.fr';
+        $mailConfirm->SMTPAuth = true;
+        $mailConfirm->Username = $_ENV['MAIL_USER'];
+        $mailConfirm->Password = $_ENV['MAIL_PASS'];
+        $mailConfirm->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;
+        $mailConfirm->Port = 465;
+        $mailConfirm->CharSet = 'UTF-8';
+        $mailConfirm->Encoding = 'base64';
+
+        $mailConfirm->setFrom('edesessard@univ-pau.fr', 'Estéban DESESSARD');
+        $mailConfirm->addAddress($_POST['email']);
+        $mailConfirm->isHTML(true);
+        $mailConfirm->Subject = "Confirmation de l'envoi de votre message";
+        $mailConfirm->Body = "Bonjour " . $_POST['prenom'] . ",<br><br>Nous avons bien reçu votre message et nous vous remercions de nous avoir contactés. Nous vous recontacterons dans les plus brefs délais.<br><br>Cordialement,<br>Expenergie";
+
+        if ($mail->send() && $mailConfirm->send()) {
+            echo $this->getTwig()->render('contact.twig', ['success' => 'Votre message a bien été envoyé. Nous vous recontacterons dans les plus brefs délais.']);
+        }
+        else {
+            echo $this->getTwig()->render('contact.twig', ['error' => 'Une erreur est survenue lors de l\'envoi du message. Merci de nous contacter directement à l\'adresse e-mail sebastien.desessard@expenergie.fr']);
+        }
+        exit;
     }
 }
--- src/routes.php
+++ src/routes.php
@@ -46,61 +46,7 @@ $router->get('/contact', function () use ($loader, $twig) {
 });
 
 $router->post('/contact', function () use ($loader, $twig) {
-    if (empty($_POST['nom']) || empty($_POST['prenom']) || empty($_POST['zipcode']) || empty($_POST['ville']) || empty($_POST['email']) || empty($_POST['message'] || !isset($_POST['accept']))) {
-        echo $twig->render('contact.twig', ['error' => 'Tous les champs sont obligatoires.']);
-        exit;
-    }
-    if (!filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
-        echo $twig->render('contact.twig', ['error' => 'L\'adresse email n\'est pas valide.']);
-        exit;
-    }
-    if (strlen($_POST['message']) < 32) {
-        echo $twig->render('contact.twig', ['error' => 'Le message doit contenir au moins 32 caractères.']);
-        exit;
-    }
-    $mail = new PHPMailer();
-    $mail->isSMTP();
-    $mail->SMTPDebug = 0;
-    $mail->Host = 'partage.univ-pau.fr';
-    $mail->SMTPAuth = true;
-    $mail->Username = $_ENV['MAIL_USER'];
-    $mail->Password = $_ENV['MAIL_PASS'];
-    $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;
-    $mail->Port = 465;
-    $mail->CharSet = 'UTF-8';
-    $mail->Encoding = 'base64';
-
-    $mail->setFrom('edesessard@univ-pau.fr', $_POST['nom'] . ' ' . $_POST['prenom']);
-    $mail->addAddress('edesessard@iutbayonne.univ-pau.fr', 'Estéban DESESSARD');
-    $mail->isHTML(true);
-    $mail->Subject = "Nouveau message depuis le formulaire de contact";
-    $mail->Body = "Message reçu de : " . $_POST['nom'] . " " . $_POST['prenom'] . " (" . $_POST['email'] . ")<br>Code postal : " . $_POST['zipcode'] . ", Ville : " . $_POST['ville'] . "<br><br>" . nl2br(htmlspecialchars($_POST['message'] ));
-    $mail->setLanguage('fr', './vendor/phpmailer/phpmailer/language/phpmailer.lang-fr.php');
-
-    $mailConfirm = new PHPMailer();
-    $mailConfirm->isSMTP();
-    $mailConfirm->SMTPDebug = 0;
-    $mailConfirm->Host = 'partage.univ-pau.fr';
-    $mailConfirm->SMTPAuth = true;
-    $mailConfirm->Username = $_ENV['MAIL_USER'];
-    $mailConfirm->Password = $_ENV['MAIL_PASS'];
-    $mailConfirm->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;
-    $mailConfirm->Port = 465;
-    $mailConfirm->CharSet = 'UTF-8';
-    $mailConfirm->Encoding = 'base64';
-
-    $mailConfirm->setFrom('edesessard@univ-pau.fr', 'Estéban DESESSARD');
-    $mailConfirm->addAddress($_POST['email']);
-    $mailConfirm->isHTML(true);
-    $mailConfirm->Subject = "Confirmation de l'envoi de votre message";
-    $mailConfirm->Body = "Bonjour " . $_POST['prenom'] . ",<br><br>Nous avons bien reçu votre message et nous vous remercions de nous avoir contacté. Nous vous recontacterons dans les plus brefs délais.<br><br>Cordialement,<br>Expenergie";
-
-    if ($mail->send() && $mailConfirm->send()) {
-        echo $twig->render('contact.twig', ['success' => 'Votre message a bien été envoyé. Nous vous recontacterons dans les plus brefs délais.']);
-    }
-    else {
-        echo $twig->render('contact.twig', ['error' => 'Une erreur est survenue lors de l\'envoi du message. Merci de nous contacter directement à l\'adresse e-mail sebastien.desessard@expenergie.fr']);
-    }
+    ControllerFactory::getController("contact", $loader, $twig)->call("sendContactForm");
     exit;
 });
 

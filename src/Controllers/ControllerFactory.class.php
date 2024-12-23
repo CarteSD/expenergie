84f9619d3e6eeb007f5c2d159d8e1469c40ab28b
Estéban DESESSARD <edesessard@iutbayonne.univ-pau.fr>
2024-12-23 13:59:41 +0100

    :bug: Correction du namespace des contrôleurs

--- src/Controllers/ControllerFactory.class.php
+++ src/Controllers/ControllerFactory.class.php
@@ -8,14 +8,16 @@
  * @version  1.0
  */
 
-use Controllers\Controller;
+namespace Controllers;
+
+use Exception;
 use Twig\Environment;
 use Twig\Loader\FilesystemLoader;
 
 class ControllerFactory {
     public static function getController(string $controller, FilesystemLoader $loader, Environment $twig): Controller
     {
-        $controllerName = 'Controller' . ucfirst($controller);
+        $controllerName = 'Controllers\Controller' . ucfirst($controller);
 
         if (!class_exists($controllerName)) {
             throw new Exception('Controller ' . $controllerName . ' not found');
